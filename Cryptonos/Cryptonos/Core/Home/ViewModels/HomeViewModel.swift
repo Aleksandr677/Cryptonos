6e2a705e4c7f751f9b5e5f5cd7951e152c9cc91b
Alexander677 <alexanderhristichenko@yandex.ru>
2023-02-08 21:38:19 +0300

    feat: add HomeViewNo

---
+++ Cryptonos/Cryptonos/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  Cryptonos
+//
+//  Created by Христиченко Александр on 2023-02-08.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
--- Cryptonos/Cryptonos/CryptonosApp.swift
+++ Cryptonos/Cryptonos/CryptonosApp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 @main
 struct CryptonosApp: App {
+    @StateObject private var vm = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 HomeView()
                     .navigationBarBackButtonHidden()
             }
+            .environmentObject(vm)
         }
     }
 }
