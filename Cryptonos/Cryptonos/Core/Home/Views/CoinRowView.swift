0511dabee1f68ae0c7568eff446237712d460b73
Alexander677 <alexanderhristichenko@yandex.ru>
2023-02-08 21:00:06 +0300

    feat: add CoinRowView. Add extentions PreviewProvider, Dounble. fix: CircleButtonView

---
+++ Cryptonos/Cryptonos/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,84 @@
+//
+//  CoinRowView.swift
+//  Cryptonos
+//
+//  Created by Христиченко Александр on 2023-02-08.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    //MARK: - PROPERTIES
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+    
+    //MARK: - BODY
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            if showHoldingsColumn {
+                centerColumn
+            } //IF
+            rightColumn
+        } //HStack
+        .font(.subheadline)
+    }
+}
+
+//MARK: - PREVIEW
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+//MARK: - EXTENTION
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text(String(coin.rank))
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        } //VStack
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green :
+                    Color.theme.red
+                )
+        } //VStack
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
