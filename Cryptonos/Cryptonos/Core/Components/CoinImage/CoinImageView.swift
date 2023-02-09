84395a58c08f62a30b5396fa937888a2960080c3
Alexander677 <alexanderhristichenko@yandex.ru>
2023-02-09 11:29:06 +0300

    feat: add CoinImageService, CoinImageView, CoinImageViewModel

---
+++ Cryptonos/Cryptonos/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,41 @@
+//
+//  CoinImageView.swift
+//  Cryptonos
+//
+//  Created by Христиченко Александр on 2023-02-09.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    //MARK: - PROPERTIES
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    //MARK: - BODY
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+//MARK: - PREVIEW
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView(coin: dev.coin)
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
