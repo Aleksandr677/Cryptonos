f0968d733392e963975676db3606f5815516fa05
Alexander677 <alexanderhristichenko@yandex.ru>
2023-02-14 20:58:17 +0300

    feat: add PortfolioDataService, PortfolioContainer

--- Cryptonos/Cryptonos/Core/Home/Views/PortfolioView.swift
+++ Cryptonos/Cryptonos/Core/Home/Views/PortfolioView.swift
@@ -38,6 +38,11 @@ struct PortfolioView: View {
                     trailingNavBarButton
                 }
             }
+            .onChange(of: vm.searchText) { newValue in
+                if newValue == "" {
+                    removeSelectedCoin()
+                }
+            }
         } //NavigationView
     }
 }
@@ -55,13 +60,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 74)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background(
@@ -72,6 +77,17 @@ extension PortfolioView {
             }
             .frame(height: 130)
             .padding(.leading)
+        }
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == coin.id }),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = String(amount)
+        } else {
+            quantityText = ""
         }
     }
     
@@ -136,10 +152,13 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else { return }
+        guard
+            let coin = selectedCoin,
+            let amount = Double(quantityText)
+        else { return }
         
         //save to portfolio
-        
+        vm.updatePortfolio(coin: coin, amount: amount)
         
         //show checkmark
         withAnimation(.easeIn) {
